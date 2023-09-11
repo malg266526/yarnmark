0966722fb54b84fbe7bbcfba26821021117429a7
Jerzy Mankowski <jmankowsk@gmail.com>
2023-09-11 19:37:21 +0200

    initial setup

---
+++ .eslintrc.js
@@ -0,0 +1,65 @@
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings'
+  ],
+  plugins: ['@typescript-eslint', 'prettier', 'import', 'react', 'typescript', 'react-hooks'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+      modules: true
+    }
+  },
+  env: {
+    browser: true
+  },
+  settings: {
+    'import/resolver': {
+      webpack: {
+        config: './webpack.config.js'
+      }
+    },
+    'import/ignore': ['node_modules'],
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.js', '.jsx', '.ts', '.tsx']
+    },
+    'import/extensions': ['.js', '.jsx', '.ts', '.tsx'],
+    react: {
+      version: 'detect'
+    }
+  },
+  rules: {
+    'no-unused-vars': 0,
+    'no-undef': 0,
+    'no-debugger': 1,
+
+    'prettier/prettier': 'error',
+    quotes: ['error', 'single'],
+    'react/no-unescaped-entities': 0,
+    'react/jsx-tag-spacing': [
+      2,
+      {
+        closingSlash: 'never',
+        beforeSelfClosing: 'always',
+        afterOpening: 'never',
+        beforeClosing: 'allow'
+      }
+    ],
+
+    '@typescript-eslint/no-unused-vars': 2,
+    // 'typescript/no-undef': 2,
+    'import/no-unused-modules': 0,
+    'import/group-exports': 0,
+    'react/jsx-key': 1,
+
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn'
+  }
+};
---
+++ webpack.config.js
@@ -0,0 +1,68 @@
+var path = require('path');
+
+var HtmlWebpackPlugin = require('html-webpack-plugin'),
+  MiniCssExtractPlugin = require('mini-css-extract-plugin'),
+  CopyPlugin = require('copy-webpack-plugin');
+
+
+
+module.exports = (env, argv) => ({
+  devtool: false,
+  cache: false,
+  target: 'web',
+  entry: './src/index',
+  output: {
+    filename: '[name].js',
+    chunkFilename: '[name].chunk.js',
+    pathinfo: true,
+    path: path.join(__dirname, './build')
+  },
+  devServer: {
+    port: 8090,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./index.ejs",
+      inject: true,
+      meta: { viewport: 'width=device-width, initial-scale=1, shrink-to-fit=no' }
+    }),
+    // new CopyPlugin([
+    //   {
+    //     from: "./src/**/*",
+    //     to: "./build",
+    //     test: /\.(svg|txt|woff|woff2|jpg)$/
+    //   }
+    // ]),
+  ],
+  module: {
+    rules: [
+      (argv.mode === 'production' ? {
+        test: /\.(js|tsx)$/,
+        exclude: /node_modules/,
+        loader: 'eslint-loader',
+        options: {
+          quiet: true
+        }
+      } : {}),
+      {
+        test: /\.(tsx?|js)$/,
+        exclude: /node_modules/,
+        loader: 'ts-loader'
+      },
+      // {
+      //   test: /\.svg$/,
+      //   loader: 'svg-inline-loader'
+      // },
+      {
+        test: /\.(woff|woff2|svg|png|mp4)$/,
+        loader: 'file-loader',
+        options: {
+          esModule: false
+        }
+      },
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js']
+  }
+});
