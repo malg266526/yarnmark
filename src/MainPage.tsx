0aa5a3b9abb48b76e00bd26d36e07d6972b3eee7
Malgo <tylek.malgorzata@interia.pl>
2023-09-11 20:55:03 +0200

    add main page content & icons library

--- src/MainPage.tsx
+++ src/MainPage.tsx
@@ -5,7 +5,12 @@ import {Header} from './components/Header';
 export const MainPage = () => (
   <div>
     <Header />
-    Yarnmark
+    <h1>Yarnmark</h1>
+    <h2> Kraków</h2>
+    <h3> 27.04</h3>
+    <h3> Stara Zajezdnia</h3>
+    <h4>Świętego Wawrzyńca 12</h4>
+
     <a href="https://google.com" target="_blank" rel="noreferrer">
       <Button> Kup Bilet </Button>
     </a>
