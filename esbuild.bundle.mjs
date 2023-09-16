d640ffe0e5a3bc86490ddd70c9c681f58e416918
mankowsj <22179350+mankowsj@users.noreply.github.com>
2023-09-16 20:04:49 +0200

    Bundle size comparison workflow (#17)
    * chore: name change for cache cleanup workflow
    
    * chore: added eslint linebreak rule
    
    * feat: bundle size comparison on pr
    
    feat: getDirMetadata file
    
    feat: getDirMetadata file

--- esbuild.bundle.mjs
+++ esbuild.bundle.mjs
@@ -1,4 +1,18 @@
 import * as esbuild from 'esbuild'
+import fs from 'fs';
 import { config } from './esbuild.config.mjs';
+import argv from 'argv';
 
-await esbuild.build(config);
+const args = argv.option([
+  {
+    name: 'metafile',
+    type: 'string'
+  }
+]).run(process.argv).options;
+
+const metafileData = await esbuild.build(config);
+
+if (args.metafile) {
+  fs.writeFileSync(args.metafile, JSON.stringify(metafileData))
+}
+
---
+++ workflow-helpers/getDirMetadata.ts
@@ -0,0 +1,54 @@
+import fs from 'fs';
+import argv from 'argv';
+import { Metadata } from './types';
+
+const { dir, out, extensions } = argv.option([
+  {
+    name: 'dir',
+    type: 'string'
+  },
+  {
+    name: 'out',
+    type: 'string'
+  },
+  {
+    name: 'extensions',
+    type: 'csv,string'
+  }
+]).run(process.argv).options;
+
+console.warn('extensions', extensions);
+
+const asyncStat = async (filename: string) => new Promise<{ size: number }>((resolve, reject) => {
+  fs.stat(filename, (error: unknown, stats: { size: number }) => {
+    if (error) {
+      reject(error);
+    }
+
+    resolve(stats);
+  });
+});
+
+const extensionRegex = /(.*)(\.[^\.]*)$/;
+
+const getFilesDataFromDir = async ({ path, extensions }: { path: string; extensions: string[] }): Promise<Metadata> => {
+  const directory = (fs.readdirSync(path, { encoding: 'utf8' }) as string[]);
+  const metadata = {} as Record<string, number>;
+
+  for await (const filename of directory) {
+    if (extensions.includes(filename.replace(extensionRegex, '$2'))) {
+      const { size } = await asyncStat(`${path}/${filename}`);
+      metadata[filename] = size;
+    }
+  }
+
+  return metadata;
+}
+
+const result = await getFilesDataFromDir({ path: dir, extensions });
+
+if (out) {
+  fs.writeFileSync(out, JSON.stringify(result));
+}
+
+export { };
