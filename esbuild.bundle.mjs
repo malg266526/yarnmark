be3368ee4fe2cf257662aee1e053b0f8601fabdb
Jerzy Mankowski <jmankowsk@gmail.com>
2023-10-22 09:40:03 +0200

    fix: added netlify redirects file to address routing issues

--- esbuild.bundle.mjs
+++ esbuild.bundle.mjs
@@ -2,6 +2,7 @@ import * as esbuild from 'esbuild'
 import fs from 'fs';
 import { config } from './esbuild.config.mjs';
 import argv from 'argv';
+import { copy } from 'esbuild-plugin-copy';
 
 const args = argv.option([
   {
@@ -10,7 +11,22 @@ const args = argv.option([
   }
 ]).run(process.argv).options;
 
-const metafileData = await esbuild.build(config);
+const metafileData = await esbuild.build({
+  ...config,
+  plugins: [
+    ...config.plugins,
+    copy({
+      // this is equal to process.cwd(), which means we use cwd path as base path to resolve `to` path
+      // if not specified, this plugin uses ESBuild.build outdir/outfile options as base path.
+      resolveFrom: 'cwd',
+      assets: {
+        from: ['./_redirects'],
+        to: ['./dist'],
+      },
+      watch: true,
+    }),
+  ]
+});
 
 if (args.metafile) {
   fs.writeFileSync(args.metafile, JSON.stringify(metafileData))
