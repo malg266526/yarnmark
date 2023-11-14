d1fb2f658f6024520bc3b13a0748432cf96f91a7
Malgo <tylek.malgorzata@interia.pl>
2023-11-14 22:53:04 +0100

    fix: add missing color

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -5,7 +5,7 @@ export const Colors = {
 
   honeyMustard: '#ae8d4a', //174,141,74
   spruce: '#095e41', // 9, 94, 65
-
+  goldLight: '#c3a25d',
   pinball: '#d3d3d3',
   white: 'white',
   ruinedSmores: '#0f0e17'
