0faaff8a4b2c0488773f1ea598a385c81d7f1892
Malgo <tylek.malgorzata@interia.pl>
2024-01-12 11:20:26 +0100

    prettier

--- src/pages/InfoForVendorsPage.tsx
+++ src/pages/InfoForVendorsPage.tsx
@@ -187,7 +187,7 @@ export const InfoForVendorsPage = () => {
         </PlainInfo>
       </Band>
 
- {/*      <Band size="sm" variant="background" justify="center" align="center" color={Colors.snow} padding="xl">
+      {/*      <Band size="sm" variant="background" justify="center" align="center" color={Colors.snow} padding="xl">
         <TitleWrapper>
           <CenteredTitle>{t('infoForVendorsPage.registration.title')}</CenteredTitle>
         </TitleWrapper>
