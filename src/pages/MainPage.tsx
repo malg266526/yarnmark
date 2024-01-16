f341011431117a674a264bad10af4f7d29762c08
Jerzy Mankowski <jmankowsk@gmail.com>
2024-01-16 23:12:35 +0100

    feat: images optimizations

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -159,7 +159,7 @@ export const MainPage = () => {
 
   // const activeButtonToImage = getActiveButtonToImage(t);
 
-  const observerCallback = useCallback(() => { }, []);
+  const observerCallback = useCallback(() => {}, []);
 
   useRootIntersectionObserver({
     rootRef: pageContentRef,
