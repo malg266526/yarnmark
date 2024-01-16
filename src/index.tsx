1ae6e26e123812bcc60b61f2a75bcf34dbc0be49
Jerzy Mankowski <jmankowsk@gmail.com>
2024-01-16 17:58:15 +0100

    chore: lint

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,11 @@ import { createRoot } from 'react-dom/client';
 import { App } from './App';
 import { I18nextProvider } from 'react-i18next';
 import { BrowserRouter } from 'react-router-dom';
-import { StyleSheetManager } from 'styled-components';  
+import { StyleSheetManager } from 'styled-components';
 import i18next from 'i18next';
 import isPropValid from '@emotion/is-prop-valid';
 import { en } from './translations/en';
 import { pl } from './translations/pl';
-
 
 i18next.init({
   interpolation: { escapeValue: false }, // React already does escaping
@@ -23,9 +22,9 @@ const root = createRoot(document.querySelector('#app')!);
 
 // This implements the default behavior from styled-components v5
 function shouldForwardProp(propName: string, target: unknown) {
-  if (typeof target === "string") {
-      // For HTML elements, forward the prop if it is a valid HTML attribute
-      return isPropValid(propName);
+  if (typeof target === 'string') {
+    // For HTML elements, forward the prop if it is a valid HTML attribute
+    return isPropValid(propName);
   }
   // For other elements, forward all props
   return true;
