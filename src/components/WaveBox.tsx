93735691197c9e128fced413137d46d3f0b2ce17
Malgo <tylek.malgorzata@interia.pl>
2024-01-31 15:57:20 +0100

    clip path

--- src/components/WaveBox.tsx
+++ src/components/WaveBox.tsx
@@ -1,20 +1,23 @@
 import React, { ReactNode } from 'react';
 import styled from 'styled-components';
 import { Spacings } from '../styles/spacings';
-import wave from '../assets/images/wave.svg';
+import { WaveImage } from './WaveImage';
 
 const Root = styled.div<{ wide?: boolean }>`
   display: flex;
   flex-direction: column;
   background-color: white;
-  padding: ${Spacings.lg};
-  padding-bottom: ${({ wide }) => (wide ? Spacings.xxl : Spacings.xl)};
   max-width: ${({ wide }) => (wide ? '25%' : '20%')};
-  border-radius: 10%;
-  align-items: flex-end;
-  background: url(${wave}) no-repeat bottom white;
-  gap: ${Spacings.sm};
+  border-radius: 4px;
   box-shadow: 0px 0px 3px 0px rgba(0, 0, 0, 0.5);
+`;
+
+const Content = styled.div<{ wide?: boolean }>`
+  display: flex;
+  flex-direction: column;
+  padding: ${Spacings.lg} ${Spacings.lg} 0 ${Spacings.lg};
+  align-items: flex-end;
+  gap: ${Spacings.sm};
 `;
 
 type WaveBoxType = {
@@ -22,4 +25,9 @@ type WaveBoxType = {
   wide?: boolean;
 };
 
-export const WaveBox = ({ children, wide }: WaveBoxType) => <Root wide={wide}>{children}</Root>;
+export const WaveBox = ({ children, wide }: WaveBoxType) => (
+  <Root wide={wide}>
+    <Content>{children}</Content>
+    <WaveImage />
+  </Root>
+);
