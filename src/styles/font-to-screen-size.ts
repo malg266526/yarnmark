314477b359d03b3b7b8bc7732ec782cf4d546ce3
Jerzy Mankowski <jmankowsk@gmail.com>
2024-02-12 18:24:41 +0100

    feat: better scaling text on mobile

---
+++ src/styles/font-to-screen-size.ts
@@ -0,0 +1,8 @@
+import { ScreenSize } from "./screeen-size";
+
+export const FontToScreenSize: Record<keyof typeof ScreenSize, string> = {
+  pc: '16px',
+  smallPc: '16px',
+  tablet: '16px',
+  phone: '14px',
+}
