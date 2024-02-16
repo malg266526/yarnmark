13f0904273f7dfc7866d6ab855ae8a9a12a98df1
Malgo <tylek.malgorzata@interia.pl>
2024-02-16 16:04:08 +0100

    add german translations

--- src/pages/InfoForVendorsPage.tsx
+++ src/pages/InfoForVendorsPage.tsx
@@ -273,7 +273,7 @@ export const InfoForVendorsPage = () => {
         color={Colors.snow}
         padding="xl">
         <TextWrapper>
-          <CenteredTitle>Hala</CenteredTitle>
+          <CenteredTitle>{t('infoForVendorsPage.hallInfo.title')}</CenteredTitle>
         </TextWrapper>
 
         <PlainInfo>
