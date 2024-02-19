7543ead60344998d7e6e11bc4271906b3b851ce3
mankowsj <22179350+mankowsj@users.noreply.github.com>
2024-02-19 21:04:35 +0100

    fix: better esbuild-server config (#203)

--- esbuild.serve.mjs
+++ esbuild.serve.mjs
@@ -2,13 +2,15 @@ import { config } from './esbuild.config.mjs';
 import { createServer } from 'esbuild-server';
 
 createServer({
-  ...config,
-  sourcemap: true
-}, {
-  port: 8090,
-  open: true,
-  injectLiveReload: true,
-  static: 'dist',
-  historyApiFallback: true,
-  sourcemap: 'inline'
-}).start();
+  sourcemap: true,
+  ...appConfig,
+},
+  {
+    port: 8090,
+    open: true,
+    injectLiveReload: true,
+    static: 'dist',
+    historyApiFallback: true,
+    sourcemap: 'inline',
+  }
+).start();
