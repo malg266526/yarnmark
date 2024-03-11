cb62b48478ea06c9057b396f5b8cbd0d0e6e539a
Jerzy Mankowski <jmankowsk@gmail.com>
2024-03-11 21:37:48 +0100

    chore: refactoring

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -4,41 +4,6 @@
 // @ts-ignore
 const sw = self as ServiceWorkerGlobalScope;
 
-const createClientLogger = (limit: number) => {
-  const logs: string[] = [];
-
-  const cacheLog = (message: string) => {
-    if (logs.length >= limit) {
-      logs.shift();
-    }
-    logs.push(message);
-    return logger;
-  };
-
-  const sendMessageToClient = (client: Client | MessagePort | ServiceWorker, message: string) => {
-    client.postMessage({ type: 'log', message });
-  };
-
-  const logger = {
-    info: function (message: string) {
-      cacheLog(message);
-    },
-
-    getCachedLogs: () => logs,
-    sendToClient: (client: Client | MessagePort | ServiceWorker) => {
-      if (logs.length) {
-        while (logs.length) {
-          sendMessageToClient(client, logs.shift()!);
-        }
-      }
-    }
-  };
-
-  return logger;
-};
-
-const clientLogger = createClientLogger(50);
-
 const CacheName = {
   preload: 'precache-v1',
   runtime: 'runtime'
@@ -63,10 +28,8 @@ const fetchOutputInfoData = () =>
     .then((data) => data.json())
     .then((data: unknown) => {
       if (isOutputInfoShape(data)) {
-        clientLogger.info(`fetchOutputInfoData :: ${data.toString()}`);
         return data;
       } else {
-        clientLogger.info(`fetchOutputInfoData :: failed to parse data :: ${data?.toString()}`);
         throw new Error('Failed to parse data');
       }
     });
@@ -80,7 +43,7 @@ const precacheStrategy = {
         return Promise.all(data.files.map((url) => caches.open(CacheName.preload).then((cache) => cache.add(url))));
       })
       .catch((error) => {
-        clientLogger.info(`Error on precaching :: ${error.String()}`);
+        console.error(`Error on precaching :: ${error.String()}`);
       }),
   jsCssHtmlAssets: () =>
     Promise.all(PRECACHE_URLS.map((url) => caches.open(CacheName.preload).then((cache) => cache.add(url))))
@@ -92,13 +55,12 @@ const precacheStrategy = {
         ]);
       })
       .catch((error) => {
-        clientLogger.info(`Error on precaching :: ${error.String()}`);
+        console.error(`Error on precaching :: ${error.String()}`);
       })
 } satisfies Record<string, () => Promise<void | void[]>>;
 
 // The install handler takes care of precaching the resources we always need.
 sw.addEventListener('install', (event) => {
-  clientLogger.info(`ServiceWorker:install`);
   event.waitUntil(
     cleanAllCache([CacheName.preload, CacheName.runtime]).then(() =>
       precacheStrategy.jsCssHtml().then(() => sw.skipWaiting())
@@ -107,7 +69,13 @@ sw.addEventListener('install', (event) => {
 });
 
 sw.addEventListener('activate', () => {
-  clientLogger.info(`ServiceWorker:activate`);
+  // event.waitUntil(
+  //   (async () => {
+  //     if (sw.registration.navigationPreload) {
+  //       await sw.registration.navigationPreload.enable();
+  //     }
+  //   })(),
+  // );
 });
 
 function cleanAllCache(cacheNames: string[]) {
@@ -118,42 +86,7 @@ function getAllCacheKeys(cacheName: string) {
   return caches.open(cacheName).then((cache) => cache.keys());
 }
 
-type WindowClientMessage =
-  | {
-      type: 'log';
-      variant: 'cacheKeys';
-    }
-  | { type: 'cleanCache' }
-  | { type: 'get-logs' };
-
-sw.addEventListener(
-  'message',
-  (event: { data: WindowClientMessage; source: Client | null | ServiceWorker | MessagePort }) => {
-    if (event.data.type === 'get-logs') {
-      if (event?.source) {
-        event?.source?.postMessage({ type: 'get-logs-reponse' });
-        clientLogger.sendToClient(event.source);
-      }
-    }
-
-    if (event.data.type === 'cleanCache') {
-      void cleanAllCache([CacheName.preload, CacheName.runtime]);
-    }
-
-    if (event.data.type === 'log') {
-      if (event.data.variant === 'cacheKeys') {
-        caches.open(CacheName.runtime).then((cache) => {
-          cache.keys().then((keys) => {
-            event?.source?.postMessage({ cacheKeys: keys.map((value) => value.url) });
-          });
-        });
-      }
-      return;
-    }
-  }
-);
-
-const cacheStrategy = {
+const respondStrategy = {
   networkFirst: (request) =>
     caches.match(request).then((cachedResponse) =>
       caches.open(CacheName.runtime).then((cache) =>
@@ -161,13 +94,13 @@ const cacheStrategy = {
           .then((response) => cache.put(request, response.clone()).then(() => response))
           .catch((error) => {
             if (cachedResponse) {
-              clientLogger.info(`Serving cached version :: ${request.url}`);
+              console.info(`Serving cached version :: ${request.url}`);
               return cachedResponse;
             }
 
             return Promise.all([getAllCacheKeys(CacheName.preload), getAllCacheKeys(CacheName.runtime)]).then(
               ([k1, k2]) => {
-                clientLogger.info(
+                console.error(
                   `Failed to retrieve cache on home / index :: ${k1.toString()} :: ${k2.toString()} :: originalError :: ${error.toString()}`
                 );
                 throw new Error(
@@ -192,7 +125,7 @@ const cacheStrategy = {
 
 sw.addEventListener('fetch', (event) => {
   if (event.request.url.startsWith(self.location.origin)) {
-    event.respondWith(cacheStrategy.networkFirst(event.request));
+    event.respondWith(respondStrategy.networkFirst(event.request));
   }
 });
 
