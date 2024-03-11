f2439938b0b5f7629c7768d7019b8c70956ec3ad
Malgo <tylek.malgorzata@interia.pl>
2024-03-11 14:27:08 +0100

    adjust logo

--- src/components/HallLogo.tsx
+++ src/components/HallLogo.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import { PictureType } from './Picture';
+import { usePhone } from '../pages/usePhone';
 
 export const Image = styled.img`
   object-fit: contain;
@@ -10,17 +11,22 @@ type HallLogoProps = {
   picture?: PictureType;
   alt?: string;
   width?: number;
-};
+}; 
 
 export const HallLogo = ({ picture, alt, width }: HallLogoProps) => {
+  const isPhone = usePhone();
+
   if (!picture?.fallbackUrl) {
     return;
   }
 
+  console.log('isPhone', isPhone);
+  const logoWidth = isPhone ? 28 : width || 36;
+
   return (
     <picture>
       {picture.sources?.map(({ type, url }, index) => <source key={index} srcSet={url} type={type} />)}
-      <Image src={picture.fallbackUrl} alt={alt} width={width || 36} />
+      <Image src={picture.fallbackUrl} alt={alt} width={logoWidth} />
     </picture>
   );
 };
