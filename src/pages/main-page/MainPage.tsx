8920e97832775e88f5167c940ca44161aeb4ee01
malg266526 <tylek.malgorzata@interia.pl>
2024-07-12 12:08:11 +0200

    refactor: move main page menu to separate file

--- src/pages/main-page/MainPage.tsx
+++ src/pages/main-page/MainPage.tsx
@@ -1,14 +1,5 @@
-import React, { useCallback, useRef, useState } from 'react';
-import { useTypedTranslation } from '../../translations/useTypedTranslation';
+import React, { useCallback, useRef } from 'react';
 
-import { usePhone } from '../usePhone';
-
-import { Icon as IconifyIcon } from '@iconify/react';
-import { Header } from '../../App.styled';
-import { BurgerMenu } from '../../components/BurgerMenu';
-import { Curtain } from '../../components/Curtain';
-import { SideBar } from '../../components/SideBar';
-import { LanguageSwitcher } from '../LanguageSwitcher';
 import { StyledPageContent } from './MainPage.styled';
 import { useRootIntersectionObserver } from '../useRootIntersectionObserver';
 
@@ -19,15 +10,11 @@ import { LocationSection } from './LocationSection';
 import { WorkshopsBand } from './workshops/WorkshopsBand';
 import { WorkshopsScheduleBand } from './workshops/WorkshopsScheduleBand';
 import { FoodBand } from './FoodBand';
-import { Menu } from '../../components/Menu';
 import { FirstAidBand } from './workshops/FirstAidBand';
 import { CruiseBand } from './CruiseBand';
+import { MainPageMenu } from './MainPageMenu';
 
 export const MainPage = () => {
-  const t = useTypedTranslation();
-  const isPhone = usePhone();
-  const [burgerActive, setBurgerActive] = useState(false);
-
   const pageContentRef = useRef<HTMLDivElement | null>(null);
 
   const ticketsFunnyButtonRef = useRef<HTMLDivElement | null>(null);
@@ -40,160 +27,9 @@ export const MainPage = () => {
     callback: observerCallback
   });
 
-  const closeSideBar = () => setBurgerActive(false);
-
   return (
     <StyledPageContent ref={pageContentRef} variant="wide" padding="none">
-      {isPhone && <Curtain onClick={() => setBurgerActive(false)} active={burgerActive} />}
-      {isPhone && (
-        <>
-          <Header>
-            <BurgerMenu onClick={() => setBurgerActive((prevValue) => !prevValue)} active={burgerActive} />
-          </Header>
-
-          <SideBar roundedCorners="left" active={burgerActive}>
-            <SideBar.LinkEntry
-              to="/"
-              onClick={closeSideBar}
-              subLinks={[
-                {
-                  to: '#vendors',
-                  name: t('menu.vendors'),
-                  icon: <IconifyIcon icon="bi:shop" width="24" />
-                },
-                {
-                  to: '#workshops',
-                  name: t('menu.workshops'),
-                  icon: <IconifyIcon icon="icons8:student" width="24" />
-                },
-                {
-                  to: '#cruise',
-                  name: t('menu.cruise'),
-                  icon: <IconifyIcon icon="clarity:ferry-solid" width="24" />
-                },
-                {
-                  to: '#food',
-                  name: t('buttonsBand.foodButton'),
-                  icon: <IconifyIcon icon="uil:pizza-slice" width="24" />
-                },
-                {
-                  to: '#footer',
-                  name: t('menu.contact'),
-                  icon: <IconifyIcon icon="clarity:talk-bubbles-solid" width="24" />
-                }
-              ]}>
-              <IconifyIcon icon="game-icons:wool" width="24" />
-              Yarnmark
-            </SideBar.LinkEntry>
-
-            <SideBar.LinkEntry
-              target="_blank"
-              to="https://wloczykijki.pl/pl_PL/i/Krakoski-Yarnmark-Welny/41?preview=true"
-              onClick={closeSideBar}
-              subLinks={[
-                {
-                  to: 'https://wloczykijki.pl/pl/p/Bilet-wstepu-na-targi-/2832',
-                  name: t('menu.entranceTicket'),
-                  target: '_blank',
-                  icon: <IconifyIcon icon="streamline:tickets" width="24" />
-                },
-                {
-                  to: 'https://wloczykijki.pl/pl/c/Krakoski-Yarnmark-Welny-warsztaty/358',
-                  name: t('menu.workshopTickets'),
-                  target: '_blank',
-                  icon: <IconifyIcon icon="streamline:tickets" width="24" />
-                },
-                {
-                  to: 'https://wloczykijki.pl/pl/p/Bilet-wstepu-na-targi-rejs/2833',
-                  name: t('menu.cruiseTickets'),
-                  target: '_blank',
-                  icon: <IconifyIcon icon="streamline:tickets" width="24" />
-                }
-              ]}>
-              <IconifyIcon icon="streamline:tickets" width="24" />
-              {t('menu.tickets')}
-            </SideBar.LinkEntry>
-
-            <SideBar.LinkEntry onClick={closeSideBar} to="/hall" target="_blank">
-              <IconifyIcon icon="gis:world-map" width="24" />
-              {t('buttonsBand.hallMap')}
-            </SideBar.LinkEntry>
-
-            <SideBar.LinkEntry onClick={closeSideBar} to="/info-for-vendors">
-              <IconifyIcon icon="material-symbols:info-outline" width="24" />
-              {t('menu.infoForVendors')}
-            </SideBar.LinkEntry>
-
-            <SideBar.LinkEntry to="/statutes" onClick={closeSideBar}>
-              <IconifyIcon icon="mdi:document-sign" width="24" />
-              {t('menu.statutes')}
-            </SideBar.LinkEntry>
-
-            <LanguageSwitcher />
-          </SideBar>
-        </>
-      )}
-
-      {!isPhone && (
-        <Menu>
-          <Menu.DropdownItem
-            subLinks={[
-              {
-                to: '#vendors',
-                name: t('menu.vendors')
-              },
-              {
-                to: '#workshops',
-                name: t('menu.workshops')
-              },
-              {
-                to: '#cruise',
-                name: t('menu.cruise')
-              },
-              {
-                to: '#food',
-                name: t('buttonsBand.foodButton')
-              },
-              {
-                to: '#footer',
-                name: t('menu.contact')
-              }
-            ]}>
-            Yarnmark
-          </Menu.DropdownItem>
-
-          <Menu.DropdownItem
-            subLinks={[
-              {
-                to: 'https://wloczykijki.pl/pl/p/Bilet-wstepu-na-targi-/2832',
-                name: t('menu.entranceTicket'),
-                target: '_blank'
-              },
-              {
-                to: 'https://wloczykijki.pl/pl/c/Krakoski-Yarnmark-Welny-warsztaty/358',
-                name: t('menu.workshopTickets'),
-                target: '_blank'
-              },
-              {
-                to: 'https://wloczykijki.pl/pl/p/Bilet-wstepu-na-targi-rejs/2833',
-                name: t('menu.cruiseTickets'),
-                target: '_blank'
-              }
-            ]}>
-            {t('menu.tickets')}
-          </Menu.DropdownItem>
-
-          <Menu.LinkItem to="/hall" target="_blank">
-            {t('buttonsBand.hallMap')}
-          </Menu.LinkItem>
-
-          <Menu.LinkItem to="/info-for-vendors">{t('menu.infoForVendors')}</Menu.LinkItem>
-
-          <Menu.LinkItem to="/statutes">{t('menu.statutes')}</Menu.LinkItem>
-
-          <LanguageSwitcher />
-        </Menu>
-      )}
+      <MainPageMenu />
 
       <InvitationBand />
 
