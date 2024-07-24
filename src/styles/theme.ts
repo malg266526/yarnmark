3080d5c8107ee5e84c59e0e45eea62c0aa0ea9e8
malg266526 <tylek.malgorzata@interia.pl>
2024-07-24 20:32:29 +0200

    use bands as compound components

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -2,7 +2,8 @@
 export const RedesignTheme = {};
 
 export const BackgroundColors = {
-  primary: '#E9E8E8'
+  primary: '#E9E8E8',
+  secondary: 'gba(236, 235, 235, 0.30)' // GrayScale[100] but 30%
 };
 
 export const TextColors = {
