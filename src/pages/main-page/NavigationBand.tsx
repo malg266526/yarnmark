c224ffe95b1b9f80cef1a1623e4c3b16c5934244
malg266526 <tylek.malgorzata@interia.pl>
2024-09-10 14:44:49 +0200

    mobile

--- src/pages/main-page/NavigationBand.tsx
+++ src/pages/main-page/NavigationBand.tsx
@@ -1,19 +1,25 @@
 import { BackgroundColors, TextColors } from '../../styles/theme';
 import React from 'react';
 import { useTypedTranslation } from '../../translations/useTypedTranslation';
-import { Icon, sizeToIconWidth } from '../../components/Icon';
+import { Icon } from '../../components/Icon';
 import styled from 'styled-components';
 import { Typography } from '../../components/Typography';
 import { RedesignSpacings } from '../../styles/spacings';
 import { RowLayout } from '../../components/RowLayout';
 import redMapMarkerIconUrl from '../../assets/figmaIcons/red_map_marker_icon.svg';
 import blueTicketsIconUrl from '../../assets/figmaIcons/blue_tickets_icon.svg';
+/*
 import shipIconUrl from '../../assets/figmaIcons/ship_icon.svg';
+*/
 import storeIconUrl from '../../assets/figmaIcons/store_icon.svg';
 import { Button } from '../../components/Button';
+/*
 import { Icon as IconifyIcon } from '@iconify/react';
+*/
 import { Band } from '../../components/bands/Band';
+/*
 import mapIcon from '../../assets/figmaIcons/map_icon.svg';
+*/
 
 const InfoSectionWrapper = styled.div`
   position: relative;
@@ -61,20 +67,20 @@ export const NavigationBand = () => {
           <Typography size="sm">{t('location.whereToFindUs')}</Typography>
         </IconButton>
 
-        <IconButton onClick={() => (window.location.href = '/home#cruise')}>
+        {/*        <IconButton onClick={() => (window.location.href = '/home#cruise')}>
           <Icon size="xl" zIndex={0} src={shipIconUrl} />
           <Typography size="sm">{t('tickets.cruise')}</Typography>
-        </IconButton>
+        </IconButton>*/}
 
-        <IconButton onClick={() => window.open('/hall')}>
+        {/*        <IconButton onClick={() => window.open('/hall')}>
           <Icon size="xl" zIndex={0} src={mapIcon} />
           <Typography size="sm">{t('hallMap.title')}</Typography>
-        </IconButton>
+        </IconButton>*/}
 
-        <IconButton onClick={() => (window.location.href = '/home#food')}>
+        {/*        <IconButton onClick={() => (window.location.href = '/home#food')}>
           <IconifyIcon icon="fxemoji:hamburger" width={sizeToIconWidth['xl']}></IconifyIcon>
           <Typography size="sm">{t('whereToEat')}</Typography>
-        </IconButton>
+        </IconButton>*/}
       </RowLayout>
     </Band.NarrowColumn>
   );
