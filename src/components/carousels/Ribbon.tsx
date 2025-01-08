f295ea1cef4361c139e0bc6a00dd46892f1c2cea
malg266526 <tylek.malgorzata@interia.pl>
2025-01-08 20:43:24 +0100

    fix vertical ribbon design

--- src/components/carousels/Ribbon.tsx
+++ src/components/carousels/Ribbon.tsx
@@ -11,7 +11,7 @@ export const Ribbon = styled.div<{ color?: string }>`
   padding-inline: calc(var(--r) + ${RedesignSpacings.md});
   clip-path: polygon(0 0, 100% 0, calc(100% - var(--r)) 50%, 100% 100%, 0 100%, var(--r) 50%);
   background: ${({ color }) => color || BackgroundColors.green.strong}; /* the main color */
-  width: '85%';
+  width: 85%;
 
   padding: ${RedesignSpacings.xs} 0;
   text-align: center;
