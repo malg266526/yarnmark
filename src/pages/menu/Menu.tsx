b8191d76d25c7ed905f369b232ad6abbee4722b2
malg266526 <tylek.malgorzata@interia.pl>
2025-04-15 15:50:27 +0200

    fix menu scroll

--- src/pages/menu/Menu.tsx
+++ src/pages/menu/Menu.tsx
@@ -33,7 +33,7 @@ const RootLayout = styled.div<{ isOpen?: boolean; isVisible?: boolean }>`
   flex-direction: column;
   width: ${({ isOpen, isVisible }) => (isVisible ? (isOpen ? '240px' : '76px') : 0)};
   position: fixed;
-  height: 100%;
+  height: 100vh;
   z-index: 10;
   left: 0;
   top: 0;
@@ -42,7 +42,8 @@ const RootLayout = styled.div<{ isOpen?: boolean; isVisible?: boolean }>`
   gap: ${RedesignSpacings.lg};
   padding: ${RedesignSpacings.xs} 0 ${RedesignSpacings.xxxl} 0;
   transition: all 0.1s linear;
-  overflow: auto;
+  overflow-y: auto;
+  overflow-x: hidden;
 
   @media (max-width: ${ScreenSize.phone}) {
     height: 100vh;
@@ -66,6 +67,8 @@ const Section = styled.div<{ isOpen?: boolean }>`
   padding-bottom: 15px;
   padding-left: ${({ isOpen }) => (isOpen ? RedesignSpacings.xs : 0)};
   padding-right: ${({ isOpen }) => (isOpen ? RedesignSpacings.xs : 0)};
+
+  // overflow: hidden;
 
   @media (max-width: ${ScreenSize.phone}) {
     padding: 0 8px 8px 8px;
@@ -161,7 +164,8 @@ const Dots = styled.span<{ isOpen?: boolean }>`
   background: url(${dotsStrokeIcon}) no-repeat center;
   background-size: contain;
   height: 30px;
-  width: 70px;
+  width: 72px;
+  max-width: 72px;
   margin: ${({ isOpen }) => (isOpen ? 0 : 'auto')};
 `;
 
@@ -209,7 +213,7 @@ export const Menu = ({ isVisible, closeMenu }: UpgradedMenuProps) => {
         </MenuItem>
       </Section>
 
-      <Section isOpen={isMenuExpanded}>
+      <Section isOpen={isMenuExpanded} id="links_section">
         <MenuItem href="/home#mainInfoButtons" isOpen={isMenuExpanded} aria-label="main_information_menu_item">
           <Icon size="sm" zIndex={0} src={infoIcon} />
           {isMenuExpanded && <Typography size="sm"> {t('menu.whatAndWhere')}</Typography>}
@@ -279,7 +283,7 @@ export const Menu = ({ isVisible, closeMenu }: UpgradedMenuProps) => {
           {isMenuExpanded && <Typography size="sm">{t('menu.infoForVendors')}</Typography>}
         </MenuItem>
 
-        {/*        <MenuItem href="/hall" isOpen={isMenuExpanded} aria-label="hall_map_menu_item">
+        {/*      <MenuItem href="/hall" isOpen={isMenuExpanded} aria-label="hall_map_menu_item">
           <Icon size="sm" zIndex={0} src={pinEllipseIcon} />
           {isMenuExpanded && <Typography size="sm">{t('menu.hallMap')}</Typography>}
         </MenuItem>*/}
